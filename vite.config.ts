c2efe65da97b7e7eec1a2622009532c37c01e692
cagas1995 <cagas1995@gmail.com>
2026-08-17 00:51:06 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,10 @@
 //     nitro (build-only using cloudflare as a default target), VITE_* env injection, @ path alias,
 //     React/TanStack dedupe, error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
- tanstackStart: {
-    deploymentTarget: 'static',
-    server: {
-      preset: 'github-pages',
-      entry: "server"
-    },
-  },
+  base: "/YOUR-REPOSITORY-NAME/",
+  plugins: [react()],
 });
